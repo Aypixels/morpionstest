655b0875bf186132a00eed3b25926d7c49a12c7f
Aypixels <83863116+Aypixels@users.noreply.github.com>
2021-05-08 11:34:18 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/ressources/:file', (req, res) => {
+    res.sendFile(__dirname + '/' + req.params.file)
+})
+
+app.listen(port, () => console.log(`App is listening at port ${port}`))
